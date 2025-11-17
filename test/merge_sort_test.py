3fef0790938acb455ae2dbfd2e3bd010dea76b1b
ermmmaks <maks.ermoshkin56@gmail.com>
2025-11-17 20:59:34 +0300

    Fix more typo

--- test/merge_sort_test.py
+++ test/merge_sort_test.py
@@ -9,7 +9,7 @@ test_array = [0, 9, 3, 5]
 
 
 def test_first():
-    assert m(test_array) == [0, 3, 5, 9]
+    assert merge_sort(test_array) == [0, 3, 5, 9]
 
 
 second_array = [766, 1, 9023, 63, 4248]
